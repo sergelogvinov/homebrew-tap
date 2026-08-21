a5b772db2d440964275a441c3d38812fad57e1e7
sergelogvinov <5407715+sergelogvinov@users.noreply.github.com>
2026-08-21 18:19:32 +0200

    Brew cask update for mimiops-mcp version v0.1.0

--- Casks/mimiops-mcp.rb
+++ Casks/mimiops-mcp.rb
@@ -40,6 +40,12 @@ cask "mimiops-mcp" do
     skip "Auto-generated on release."
   end
 
+  postflight do
+    if OS.mac?
+      system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/mimiops-mcp_#{os}_#{arch}"]
+    end
+  end
+
   # No zap stanza required
 
 end
